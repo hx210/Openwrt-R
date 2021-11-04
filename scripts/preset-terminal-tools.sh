e19831ff608fe52b1a4033030ebcb327c451c056
hx210 <76199340+hx210@users.noreply.github.com>
2021-11-04 15:54:14 +0800

    zsh增加历史命令搜索插件

--- scripts/preset-terminal-tools.sh
+++ scripts/preset-terminal-tools.sh
@@ -19,6 +19,7 @@ git clone https://github.com/robbyrussell/oh-my-zsh ./.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-history-substring-search ./.oh-my-zsh/custom/plugins/zsh-history-substring-search
 
 # Get .zshrc dotfile
 cp ../../../data/zsh/.zshrc .
