9aeb6d73621a438c69da443e45215ce64bb74078
hx210 <76199340+hx210@users.noreply.github.com>
2021-11-26 10:01:54 +0800

    upx 在上游配置

--- scripts/op-openwrt.sh
+++ scripts/op-openwrt.sh
@@ -99,11 +99,6 @@ popd
 
 
 
-# UPX 可执行软件压缩
-sed -i '/patchelf pkgconf/i\tools-y += ucl upx' ./tools/Makefile
-sed -i '\/autoconf\/compile :=/i\$(curdir)/upx/compile := $(curdir)/ucl/compile' ./tools/Makefile
-svn co https://github.com/immortalwrt/immortalwrt/branches/master/tools/upx tools/upx
-svn co https://github.com/immortalwrt/immortalwrt/branches/master/tools/ucl tools/ucl
 #update golang
 pushd feeds/packages/lang
 rm -rf golang && svn co https://github.com/openwrt/packages/trunk/lang/golang
